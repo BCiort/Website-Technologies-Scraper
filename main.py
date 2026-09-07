c713582c667409f986d21679854732a1497f15ab
Bogdan-Cristian Ciortea <e12530100@student.tuwien.ac.at>
2026-09-07 20:04:45 +0300

    Converted the parquet file to txt and added it to the project

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import asyncio
+import os
 import aiohttp
 from src.fetcher import fetch_domain_data
 from src.analyzer import Analyzer
 
 
+def load_domains(file_path: str) -> list:
+    if not os.path.exists(file_path):
+        print(f"[!] ERROR: File not found {file_path}")
+        return []
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip()]
+
+
 async def main():
     analyzer = Analyzer()
-    test_domains = [
-        "shopify.com",
-        "wordpress.org",
-        "react.dev",
-    ]
 
-    print("Start:\n")
+    test_domains = load_domains("domains.txt")
+
+    if not test_domains:
+        print("Domains could not be loaded.")
+        return
+
+    print(f"Start on {len(test_domains)} domains::\n")
     unique_technologies = set()
-    async with aiohttp.ClientSession() as session:
+
+    connector = aiohttp.TCPConnector(limit=20)
+    async with aiohttp.ClientSession(connector=connector) as session:
         tasks = [fetch_domain_data(session, domain) for domain in test_domains]
         results = await asyncio.gather(*tasks)
 
@@ -22,14 +35,13 @@ async def main():
             domain = res['domain']
 
             if res.get("error"):
-                print(f"[X] EROARE pentru {domain}: {res['error']}")
+                print(f"[X] Error for {domain}: {res['error']}")
                 continue
 
             found_technologies = analyzer.analyze(res)
 
             if found_technologies:
                 print(f"[!] {domain}:")
-
                 for tech in found_technologies:
                     print(f"    -> {tech['name']} ({tech['category']})")
                     unique_technologies.add(tech['name'])
